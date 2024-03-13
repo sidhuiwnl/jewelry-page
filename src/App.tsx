4d29f80420b2c466f6687030b23998b26beada90
Sidharth Babu <95430324+sidhuiwnl@users.noreply.github.com>
2024-03-13 21:58:57 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import image3 from './assets/img3.jpeg'
 import image4 from './assets/img4.jpeg'
 import image5 from './assets/img5.jpeg'
 import image6 from './assets/img6.jpeg'
+import image7 from './assets/img10.jpeg'
+import image8 from './assets/img8.jpeg'
+import image9 from './assets/img9.jpeg'
 import Navbar from "./Components/Navbar"
 import Footer from "./Components/Footer"
 
@@ -20,12 +23,15 @@ function App() {
     <IntroSection/>
     <Breaking/>
     <div className="grid grid-cols-3 p-1 m-3">
-      <JewelCard image={image1}/>
-      <JewelCard image={image2}/>
-      <JewelCard image={image3}/>
-      <JewelCard image={image4}/>
-      <JewelCard image={image5}/>
-      <JewelCard image={image6}/>
+      <JewelCard image={image1} desc={"A red color Bag"}/>
+      <JewelCard image={image2} desc={"A golden Bag"}/>
+      <JewelCard image={image3} desc={"A brown Box"}/>
+      <JewelCard image={image4} desc={"A red color Bag"}/>
+      <JewelCard image={image5}  desc={"A brown Box"}/>
+      <JewelCard image={image6} desc={"A red color Bag"}/>
+      <JewelCard image={image8} desc={"A red color Bag"}/>
+      <JewelCard image={image7}  desc={"A brown Box"}/>
+      <JewelCard image={image9}  desc={"A brown Box"}/>
 
       
     </div>
